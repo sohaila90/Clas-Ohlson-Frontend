d2bb6198585c702d40d9bc80ecd74e95585d9cc8
sohaila90 <sohaila.al-khayat@hotmail.com>
2025-09-05 09:48:32 +0200

    Update Electro.vue

--- src/components/Pages/Electro.vue
+++ src/components/Pages/Electro.vue
@@ -1,12 +1,39 @@
 <script setup>
-
+import pepperkakehus from "@/assets/pepperkakehus.avif";
+const products = [
+  { id: 1, name: "Luftrenser", price: 1999 },
+  { id: 2, name: "Utelampe", price: 1199 },
+  { id: 3, name: "Overvåkningskamera", price: 2190 },
+  {
+    id: 4,
+    name: "Batteridrevet pepperkakehus",
+    price: 150,
+    image: "pepperkakehus",
+  },
+];
 </script>
 
 <template>
-<h1>Snart kommer det noe her</h1>
+  <div>
+    <h1 class="text-5xl font-bold">Elektro</h1>
+    <p>
+      God elektroprodukter gjør forskjell – fra sikker installasjon til riktig
+      lys og smarte løsninger. Vi vet hvor viktig det er å lyse opp både hverdag
+      og høytid, både sommer og vinter. Oppdag produkter som gir trygghet,
+      komfort og kontroll, slik at du kan ta vare på det du liker – og få det
+      til å vare!
+    </p>
 
+    <ul v-for="product in products">
+      <img
+        :src="product.image"
+        alt="pepperkakehus"
+        class="w-40 h-40 object-cover"
+      />
+      <li>{{ product.name }}</li>
+      <li>{{ product.price }}</li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
