6ec551d026774aafe4fda64a3ece4a446b87dd20
sohaila90 <sohaila.al-khayat@hotmail.com>
2025-09-29 14:10:39 +0200

    Update ClasFront.vue

--- src/components/Layouts/ClasFront.vue
+++ src/components/Layouts/ClasFront.vue
@@ -6,7 +6,7 @@ const showCart = ref(false);
 </script>
 
 <template>
-  <section id="dashboard" class="flex justify-between items-center p-4 bg-gray-100">
+  <section id="dashboard" class="flex justify-between items-center p-4 bg-blue-500">
 
     <img class="h-12" src="/src/assets/Clas_Ohlson.svg (1).png" />
     <div class="relative">
@@ -72,13 +72,41 @@ const showCart = ref(false);
   </section>
 
     <div class="card">
-      <h2>Velkommen</h2>
+    
     </div>
+
   <div>
+
+    <div class="relative w-full max-w-lg mx-auto">
+      <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400">
+        <!-- Forstørrelseglass-ikon -->
+         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35m0 0A7.5 7.5 + 1110.5 3a7.5 7.5 0 016.15 13.65z" />
+         </svg>
+      </span>
     <input
       type="text"
       placeholder="Hva ser du etter i dag?"
-      class="text-pink px-4 py-2 rounded"
+      class="
+      w-full
+      pl-10
+      pr-4
+      py-2
+      rounded-lg
+      border
+      border-gray-300
+      bg-white
+      text-gray-800
+      placeholder-gray-400
+      shadow-sm
+      focus:outline-none
+      focus:ring-2
+      focus:ring-[#003DA5]
+      focus:border-[#003DA5]
+      transition
+      duration-200
+      hover:border-gray-400
+      "
     />
     <div>
       <button
@@ -87,6 +115,7 @@ const showCart = ref(false);
       >
         Søk
       </button>
+      </div>
     </div>
   </div>
 </template>
